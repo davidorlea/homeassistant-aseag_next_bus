2b5431a0062b4fd5cfa23d4a8d6c8f5b7fa20b6b
David Orlea <david.orlea@gmail.com>
2024-06-21 19:02:28 +0200

    Add test setup with dummy test

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for ASEAG Next Bus Sensor."""
---
+++ tests/test_sensor.py
@@ -0,0 +1,8 @@
+"""Tests for ASEAG Next Bus Sensor."""
+
+import requests_mock
+
+
+def test_dummy(requests_mock: requests_mock.Mocker):
+    """Dummy test."""
+    assert True
